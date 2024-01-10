f4d83b51a3e0479c421d4d79d993997dd17b7025
Raphael Beaudet <raphaelbeaudet@icloud.com>
2024-01-10 15:32:20 +0100

    Initial Commit

---
+++ IA du futur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IA du futur
+//
+//  Created by Raphaël Beaudet on 10/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IA du futur/IA_du_futurApp.swift
@@ -0,0 +1,17 @@
+//
+//  IA_du_futurApp.swift
+//  IA du futur
+//
+//  Created by Raphaël Beaudet on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct IA_du_futurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
